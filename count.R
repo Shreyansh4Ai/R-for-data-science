7af7b6eff4688db434d2c9d7baca4154030cf25b
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2026-02-17 17:12:22 +0530

    r basic

---
+++ count.R
@@ -0,0 +1,8 @@
+mario <-readline("enter votes for mario :")
+peach<-readline("enter votes for peach :")
+bowser<-readline("enter votes for bowser :")
+
+
+total<- mario + peach + bowser 
+
+print(paste("Total Votes:" , total ))
