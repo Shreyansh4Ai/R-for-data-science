ca1fd2119c0cef70802064e483c42ee357e532d8
Shreyansh Goswami <goswami121shreyansh@gmail.com>
2026-04-22 15:40:38 +0530

    Create positiveNegative.r

---
+++ positiveNegative.r
@@ -0,0 +1,12 @@
+                          
+x<-scan(what = numeric() , quiet = TRUE)
+
+if(x>0){
+  print("Positive")
+}else if(x<0){
+  print("Negative")
+}else{
+  print("zero")
+}
+
+# bracket k just bhagal m hi else likhna tabhi kaam krega 
